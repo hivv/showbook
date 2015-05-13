8e21e623e9d81f4503f421ed06cf2e796b07f302
hivv <tristan.velardo@insa-lyon.fr>
2015-05-13 09:12:32 +0200

    signin

---
+++ app/static/signin.js
@@ -0,0 +1,54 @@
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+function verifPseudo(champ)
+{
+   if(champ.value.length < 2 || champ.value.length > 25)
+   {
+      surligne(champ, true);
+      affichPseudo("pseudo pas bien");
+      //alert("Votre pseudo doit contenir entr 2 et 25 caractères");
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifYear(champ)
+{
+   var age = parseInt(champ.value);
+   if(isNaN(age) || age < 1918|| age > 1997)
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+

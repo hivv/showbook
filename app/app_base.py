5883aec8dfa2db7256694a11d4f82e360f7fa273
hivv <tristan.velardo@insa-lyon.fr>
2015-05-20 08:12:37 +0200

    Razvan's sunday

--- app/app_base.py
+++ app/app_base.py
@@ -4,7 +4,10 @@ from flask  import *
 from sqlalchemy import *
 from markdown import markdown
 import os, hashlib
+import datetime; 
 from sqlalchemy.ext.automap import automap_base
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
 
 # ............................................................................................... #
 
@@ -31,19 +34,30 @@ events = Table('events', metadata,
 	Column('id', Integer, primary_key=True),
 	Column('name', String, nullable=False),
 	Column('description', String),
-	Column('date', DateTime),
+	Column('date', Date),
+	Column('img',String, nullable=False),
 	Column('salle', Integer, ForeignKey('salles.id')),
 	sqlite_autoincrement=True)
 
 salles = Table('salles', metadata,
 	Column('id', Integer, primary_key=True),
-	Column('numero', String, nullable=False),
+	Column('name', String, nullable=False),
+	Column('link',String,nullable=False),
+	Column('plan',String, nullable=False),
+	Column('c1',String, nullable=False),
+	Column('c2',String, nullable=True),
+	Column('c3',String, nullable=True),
+	Column('n1',Integer, nullable=False),
+	Column('n2',Integer, nullable=True),
+	Column('n3',Integer, nullable=True),
 	Column('nb_places', Integer, nullable=False),
 	sqlite_autoincrement=True)
 	
 reservations = Table('reservations', metadata,
 	Column('id_Event', Integer, ForeignKey('events.id')),
 	Column('id_User', Integer, ForeignKey('users.id')),
+	Column('id_Cat', Integer, nullable=False), 
+	Column('Social', Integer, nullable=False),
 	Column('places_reserves', Integer, nullable=False))
 
 metadata.create_all(engine)
@@ -54,9 +68,15 @@ def hashFor(password):
 
 db = engine.connect();
 db.execute(users.insert().values(login='admin', password_hash=hashFor('admin')))
-db.execute(salles.insert().values(numero='S1', nb_places=50))
-db.execute(salles.insert().values(numero='S2', nb_places=10))
-db.execute(salles.insert().values(numero='S3', nb_places=20))
+db.execute(users.insert().values(login='aaa', password_hash=hashFor('aaa')))
+db.execute(salles.insert().values(name='Theatre Royal Wakefield', link='http://www.theatreroyalwakefield.co.uk',plan='/static/img/plan1.gif', c1='Stalls', c2='Dress Circle', c3='Upper Circle', n1=200, n2=150, n3=100, nb_places=450))
+db.execute(salles.insert().values(name='TBarnfield Theatre', link='http://www.barnfieldtheatre.org.uk/',plan='/static/img/plan2.jpg', c1='Unique Category', n1=300, nb_places=300))
+#db.execute(salles.insert().values(numero='S3', nb_places=20))
+db.execute(events.insert().values(name='Hearscape',description='Hearscape is cool but unknown', img='/static/img/hearscape.png',date=datetime.date(2015, 06, 21),salle=1))
+db.execute(events.insert().values(name='Rolling Stones',description='The Rolling Stones are one of the biggest Rock Band ever',img='/static/img/stones.jpg',date = datetime.date(2015,07,23),salle=2))
+db.execute(events.insert().values(name='Archive',description='Archive is an English trip-hop and progressive rock band very appreciated in France',img='/static/img/archive.jpg',date = datetime.date(2015,8,11),salle=2))
+
+
 db.close()
 
 # ............................................................................................... #
@@ -101,7 +121,6 @@ def signUp(login, nom, prenom, mail, password, date_naissance,telephone):
 	db = engine.connect()
 	try:
 		if db.execute(select([users.c.login]).where(users.c.login == login)).fetchone() is None:
-			print "Hello"
 			db.execute(users.insert().values(login=login, nom=nom, prenom=prenom, mail=mail, password_hash=hashFor(password),date_naissance=date_naissance,telephone=telephone))
 			return True
 		else:
@@ -109,6 +128,67 @@ def signUp(login, nom, prenom, mail, password, date_naissance,telephone):
 	finally:
 		db.close()
 
+def retrieveProfile(username):
+	db = engine.connect()
+	try:
+		if db.execute(select([users.c.login]).where(users.c.login == username)).fetchone() != None:
+			sel = select([users.c.nom, users.c.prenom, users.c.mail, users.c.date_naissance, users.c.telephone,users.c.login]).where(and_(users.c.login == username))
+			usr=db.execute(sel)
+			for row in usr:
+				return row
+		else :
+			return None
+	finally :
+		db.close()
+
+def findShows(username):
+	db = engine.connect()
+	try :
+		if db.execute(select([users.c.login]).where(users.c.login == username)).fetchone() != None:
+			sel = select([events.c.name, events.c.date, reservations.c.places_reserves]).where(and_(users.c.login == username, users.c.id == reservations.c.id_User, events.c.id == reservations.c.id_Event, events.c.salle == salles.c.id))
+			show = db.execute(sel)
+			for row in show :
+				return row
+		else :
+			return None
+	finally :
+		db.close()
+
+def newReservation(user, show, nbpl, soc, cat):
+	db=engine.connect()
+	print('1')
+	try:
+		db.execute(reservations.insert().values(id_Event=show,id_User=user,places_reserves=nbpl, Social=soc, id_Cat=cat))
+		print('2')
+		return True
+	finally:
+		db.close()
+
+
+					
+
+def generate_token(pseudo,expiration=600):
+	s= Serializer(app.config['SECRET_KEY'],expires_in = expiration)
+	tok = s.dumps({'Pseudo':pseudo})
+	#print verify_auth_token(tok)
+	return tok
+
+def verify_auth_token(token):
+	s=Serializer(app.config['SECRET_KEY'])
+	try:
+		userData = s.loads(token)
+		user = userData['Pseudo']
+		print user
+		return user
+	except SignatureExpired:
+		print "erreur1"
+		return None
+	except BadSignature:
+		print "erreur2"
+		return None
+	
+
+	
 #def updateUser(login, password, nom, prenom, mail, telephone, date_naissance):
 	#db = engine.connect()
 	#try:
@@ -127,7 +207,7 @@ def index():
 #def index(name='Main'):
 	#return redirect('/pages/' + name)
 
-@app.route('/pages/<name>')
+@app.route('/book/<name>')
 def page(name):
 	raw = page_content(name)
 	content = markdown(raw)
@@ -142,50 +222,128 @@ def save():
 	else:
 		delete_page(page)
 	return redirect('/pages/' + page)
-
-#@app.route('/login', methods=['GET', 'POST'])
-#def login():
-	#from_page = request.args.get('from', 'Main')
-	#if request.method == 'POST':
-		#if authenticate_or_create(request.form['login'], request.form['password']):
-			#session['username'] = request.form['login']
-			#return redirect('/pages/' + from_page)
-		#else:
-			#flash('Invalid password for login: ' + request.form['login'])
-			#return redirect('/login?from=' + from_page)
-	#else:
-		#return render_template('login.html', from_page=from_page)
+	
 
 @app.route('/login', methods=['POST'])
 def login():
-	
-	if request.method == 'POST':
-		content = request.get_json(force=True)
-		print content
-		print content['Pseudo']
-		res = authenticate(content['Pseudo'], content['Password'])
-		print "ololo"
-		print res
-		return json.dumps({'success':res[0]})
-	else : 
-		return json.dumps({'success':False}), 200, {'ContentType':'application/json'} 
+	content = request.get_json(force=True)
+	print content
+	print content['Pseudo']
+	res = authenticate(content['Pseudo'], content['Password'])
+	if res[0]:	
+		token = generate_token(content['Pseudo'])
+		return json.dumps({'success':True, 'token' : token.decode('ascii')})
+	else: 		
+		return json.dumps({'success':False, 'result':res[1]})
+ 
 
 @app.route('/signUp', methods=['POST'])
 def signup():
+	content = request.get_json(force=True)
+	print content
+	res = signUp(content['Pseudo'], content['FirstName'], content['Surname'] ,content['Email'], content['Password'], content['YearOfBirth'], content['PhoneNumber'])
+	if res :
+		tok=generate_token(content['Pseudo'])	
+		return json.dumps({'success':True,'token' : tok.decode('ascii')})
+	else :
+		return json.dumps({'success':False})
 	
-	if request.method == 'POST':
-		content = request.get_json(force=True)
-		print content
-		print content['Pseudo']
-		print content['Email']
-		res = signUp(content['Pseudo'], content['FirstName'], content['Surname'] ,content['Email'], content['Password'], content['YearOfBirth'], content['PhoneNumber'])
-		print "ololo2222"
+
+
+@app.route('/users', methods=['POST'])
+def user():
+	content = request.get_json(force=True)
+	tok = content['Token']
+	user = verify_auth_token(tok)
+	if user != None :
+		return json.dumps({'username':user})
+	else :
+		return redirect('/')
+
+@app.route('/profile',methods=['POST'])
+def profile():
+	content = request.get_json(force=True)
+	tok = content['Token']
+	user = verify_auth_token(tok)
+	if user != None :
+		res = retrieveProfile(user)
 		print res
-		return json.dumps({'success':res})
-	else : 
-		return json.dumps({'success':False}), 200, {'ContentType':'application/json'} 
+		return json.dumps({'name':res[0],'surname':res[1],'email':res[2],'date':res[3],'phone':res[4],'login':res[5]})
+	else :
+		return redirect('/')
 
+@app.route('/shows', methods=['Post'])
+def shows():
+	content = request.get_json(force=True)
+	tok = content['Token']
+	user = verify_auth_token(tok)
+	if user != None :
+		res = findShows(user)
+		print res
+		return json.dumps({'name':res[0]})
+	else :
+		return redirect('/')
+	
+@app.route('/_get_events_infos/')
+def infos():
+	db=engine.connect()
+	n = request.args.get('n', 0, type=int)
+	print('requ$ete numero')
+	print(n)
+	#db.events.order_by(db.events.date.desc().all())
+	s=select([events.c.name, events.c.date, events.c.img, events.c.id])
+	result = db.execute(s)
+	#result=result.order_by(desc(events.date)).limit(9).all()
+	i=0
+	
+	for row in result :
+		if i is n:
+			ret_data={"name": row[0], "img": row[2],"date":row[1].strftime('%d/%m/%Y'), "id":row[3]}
+			return jsonify(ret_data)
+		else:
+			i=i+1
+	db.close()
 
+@app.route('/_get_events_book/')
+def book():
+	db=engine.connect()
+	idShow=request.args.get('n',0,type=int)
+	print "uhuhuhuh",idShow
+	try :
+		s=select([events.c.name, events.c.date, events.c.img, events.c.description, events.c.salle]).where(events.c.id==idShow)
+		result=db.execute(s)
+		row_one=result.fetchone()
+		salle=select([salles.c.name, salles.c.link, salles.c.plan, salles.c.c1,salles.c.c2, salles.c.c3]).where(salles.c.id==row_one[4])
+		salle_res=db.execute(salle)
+		row_two=salle_res.fetchone()
+	finally :
+		db.close()
+	ret_data={"name":row_one[0], "date":row_one[1].strftime('%d/%m/%Y'), "img":row_one[2], "desc":row_one[3], "nomSalle":row_two[0], "link":row_two[1], "plan":row_two[2], "c1":row_two[3], "c2":row_two[4], "c3":row_two[5]}
+	return jsonify(ret_data)
+
+@app.route('/booking_engine', methods=['POST'])
+def bookEngine():
+	db=engine.connect()
+	content = request.get_json(force=True)
+	print content
+	s=select([events.c.id]).where(events.c.name==content['Show'])
+	show=db.execute(s).fetchone()
+	u=select([users.c.id]).where(users.c.login==content['Pseudo'])
+	user=db.execute(u).fetchone()
+	us=user[0]
+	sh=show[0]
+	try:
+		db.execute(reservations.insert().values(id_Event=sh,id_User=us,places_reserves=content['nbPlaces'], Social=content['social'], id_Cat=content['cat']))
+		mess='The reservation has been processed and is now visible on your profile'
+	except :
+		mess='The reservation has failed. We apologize for the error.'
+	finally:
+		db.close()
+	return json.dumps({'success':True, 'token' : "coucou", 'result':mess})
+	#else: 		
+	#	return json.dumps({'success':False, 'result':res[1]})
+
+	
 @app.route('/logout')
 def logout():
     from_page = request.args.get('from', 'Main')
@@ -195,8 +353,8 @@ def logout():
 # ............................................................................................... #
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=False)
-	
+	app.run(debug=True)
+	#use_reloader=False
 
 # ............................................................................................... #
 
---
+++ app/static/book.js
@@ -0,0 +1,51 @@
+
+
+
+function book(){
+	var catChosen=$('#catChoose').val();
+	var nbPlaces=$('#nbpl').val();
+	var social=$('#soc').val();	  
+	document.getElementById("result").innerHTML = 'Booking... Please Wait...';	  
+	  $.ajax({
+		type:"POST",
+		url: "/booking_engine",        
+		dataType: "json",
+		contentType: "application/json; charset=utf-8",
+		//data: "Pseudo="+Pseudo+"&Password="+Password,
+		data: JSON.stringify({
+			Pseudo: pseudo,
+			Show: name,
+			nbPlaces:nbPlaces,
+			cat:catChosen, 
+			social:social
+		}),
+		success: function(data){
+			document.getElementById("result").innerHTML = data['result'];
+			if (data['success']) { 
+				console.log(data['token']);
+				// Check browser support
+				if (typeof(Storage) != "undefined") {
+					// Store
+						//localStorage.setItem("token", data['token']);
+					//document.location.href="logged.html" ;
+					// Retrieve
+					//document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+				} else {
+					document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+				}
+				//document.location.href="logged.html" ;
+			}
+			else {
+				document.getElementById("result").innerHTML = data['result'];
+			}	
+		},
+		error:function(){
+			alert("une erreur est survenue");
+		},
+		default: function(){
+			alert("défaut");
+		}             
+	});
+} 
+
+
